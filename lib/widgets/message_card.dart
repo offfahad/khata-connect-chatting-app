bc3ccf5cef03018b16c5109db6d590a4394c5c00
offfahad <mughalfahad544@gmail.com>
2024-08-26 19:49:46 +0500

    message-card-done

--- lib/widgets/message_card.dart
+++ lib/widgets/message_card.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // Import Clipboard package
 import 'package:flutter_notifications/api/apis.dart';
@@ -5,6 +8,7 @@ import 'package:flutter_notifications/helpers/dialogs.dart';
 import 'package:flutter_notifications/models/chat_user.dart'; // Ensure you import the appropriate model
 import 'package:flutter_notifications/models/message.dart';
 import 'package:flutter_notifications/models/newTransactionsModel.dart';
+import 'package:gallery_saver_updated/gallery_saver.dart';
 import '../helpers/my_date_util.dart';
 
 class MessageCard extends StatelessWidget {
@@ -64,10 +68,28 @@ class MessageCard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              Text(
-                message.msg,
-                style: const TextStyle(fontSize: 12, color: Colors.black),
-              ),
+              message.type == Type.text
+                  ? Text(
+                      message.msg,
+                      style: const TextStyle(fontSize: 12, color: Colors.black),
+                    )
+                  : ClipRRect(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      child: CachedNetworkImage(
+                        imageUrl: message.msg,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) => const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CircularProgressIndicator(strokeWidth: 2),
+                        ),
+                        errorWidget: (context, url, error) => const Icon(
+                          Icons.image,
+                          size: 70,
+                        ),
+                      ),
+                    ),
               const SizedBox(height: 3),
               Row(
                 mainAxisSize: MainAxisSize.min,
@@ -153,10 +175,28 @@ class MessageCard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                message.msg,
-                style: const TextStyle(fontSize: 12, color: Colors.black),
-              ),
+              message.type == Type.text
+                  ? Text(
+                      message.msg,
+                      style: const TextStyle(fontSize: 12, color: Colors.black),
+                    )
+                  : ClipRRect(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      child: CachedNetworkImage(
+                        imageUrl: message.msg,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) => const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CircularProgressIndicator(strokeWidth: 2),
+                        ),
+                        errorWidget: (context, url, error) => const Icon(
+                          Icons.image,
+                          size: 70,
+                        ),
+                      ),
+                    ),
               const SizedBox(height: 4),
               Text(
                 MyDateUtil.getFormattedTime(
@@ -178,103 +218,166 @@ class MessageCard extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Wrap(
-              children: isCurrentUser
-                  ? [
-                      ListTile(
-                        leading: const Icon(
-                          Icons.copy_all_rounded,
-                          color: Colors.blue,
-                        ),
-                        title: const Text('Copy'),
-                        onTap: () async {
-                          // Copy the message text to clipboard
-                          await Clipboard.setData(
-                              ClipboardData(text: message.msg));
-                          // Show a snackbar to confirm the copy action
-                          Navigator.pop(context); // Hide the bottom sheet
-                          Dialogs.showSnackbar(context, 'Message Copied!');
-                        },
+              children: [
+                if (message.type == Type.image)
+                  // Show Save Image options if the message contains an image
+                  ...[
+                  ListTile(
+                    leading: const Icon(
+                      Icons.download,
+                      color: Colors.blue,
+                    ),
+                    title: const Text('Save Image'),
+                    onTap: () async {
+                      try {
+                        log('Image Url: ${message.msg}');
+                        bool? success = await GallerySaver.saveImage(
+                          message.msg,
+                          albumName: 'Khata Connect',
+                        );
+                        Navigator.pop(context); // Hide the bottom sheet
+                        if (success != null && success) {
+                          Dialogs.showSnackbar(
+                              context, 'Image Successfully Saved!');
+                        }
+                      } catch (e) {
+                        log('Error While Saving Image: $e');
+                        Dialogs.showSnackbar(context, 'Error Saving Image');
+                      }
+                    },
+                  ),
+                  if (isCurrentUser)
+                    ListTile(
+                      leading: const Icon(
+                        Icons.delete,
+                        color: Colors.red,
                       ),
-                      ListTile(
-                        leading: const Icon(
-                          Icons.edit,
-                          color: Colors.blue,
-                        ),
-                        title: const Text('Edit'),
-                        onTap: () {
-                          // Implement edit functionality
-                          _showEditMessageDialog(context);
-                        },
-                      ),
-                      ListTile(
-                        leading: const Icon(
-                          Icons.delete,
-                          color: Colors.red,
-                        ),
-                        title: const Text('Delete'),
-                        onTap: () async {
-                          // Call delete function
-                          await APIs.deleteMessage(message);
-                          Navigator.pop(context);
-                        },
-                      ),
-                      const Divider(),
-                      ListTile(
-                        leading: const Icon(
-                          Icons.remove_red_eye,
-                          color: Colors.green,
-                        ),
-                        title: Text(
-                            'Sent At: ${MyDateUtil.getFormattedTime(context: context, time: message.sent)}'),
-                      ),
-                      ListTile(
-                        leading: const Icon(
-                          Icons.remove_red_eye,
-                          color: Colors.blue,
-                        ),
-                        title: Text(
-                            'Read At: ${message.read.isNotEmpty ? '${MyDateUtil.getMessageTime(context: context, time: message.read)}' : 'Not seen yet'}'),
-                      ),
-                    ]
-                  : [
-                      ListTile(
-                        leading: const Icon(
-                          Icons.copy_all_rounded,
-                          color: Colors.blue,
-                        ),
-                        title: const Text('Copy'),
-                        onTap: () async {
-                          // Copy the message text to clipboard
-                          await Clipboard.setData(
-                              ClipboardData(text: message.msg));
-                          // Show a snackbar to confirm the copy action
-                          Navigator.pop(context); // Hide the bottom sheet
-                          Dialogs.showSnackbar(context, 'Message Copied!');
-                        },
-                      ),
-                      const Divider(),
-                      ListTile(
-                        leading: const Icon(
-                          Icons.remove_red_eye,
-                          color: Colors.green,
-                        ),
-                        title: Text(
-                            'Sent At: ${MyDateUtil.getFormattedTime(context: context, time: message.sent)}'),
-                      ),
-                      ListTile(
-                        leading: const Icon(
-                          Icons.remove_red_eye,
-                          color: Colors.blue,
-                        ),
-                        title: Text(
-                            'Read At: ${message.read.isNotEmpty ? '${MyDateUtil.getMessageTime(context: context, time: message.read)}' : 'Not seen yet'}'),
-                      ),
-                    ],
+                      title: const Text('Delete'),
+                      onTap: () async {
+                        // Implement the delete functionality
+                        await APIs.deleteMessage(message);
+                        Navigator.pop(context); // Hide the bottom sheet
+                        Dialogs.showSnackbar(context, 'Image Deleted!');
+                      },
+                    ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.green,
+                    ),
+                    title: Text(
+                        'Sent At: ${MyDateUtil.getFormattedTime(context: context, time: message.sent)}'),
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                        'Read At: ${message.read.isNotEmpty ? '${MyDateUtil.getMessageTime(context: context, time: message.read)}' : 'Not seen yet'}'),
+                  ),
+                ] else
+                // Default options if the message does not contain an image
+                if (isCurrentUser) ...[
+                  ListTile(
+                    leading: const Icon(
+                      Icons.copy_all_rounded,
+                      color: Colors.blue,
+                    ),
+                    title: const Text('Copy'),
+                    onTap: () async {
+                      // Copy the message text to clipboard
+                      await Clipboard.setData(ClipboardData(text: message.msg));
+                      // Show a snackbar to confirm the copy action
+                      Navigator.pop(context); // Hide the bottom sheet
+                      Dialogs.showSnackbar(context, 'Message Copied!');
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.edit,
+                      color: Colors.blue,
+                    ),
+                    title: const Text('Edit'),
+                    onTap: () {
+                      // Implement edit functionality
+                      _showEditMessageDialog(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                    ),
+                    title: const Text('Delete'),
+                    onTap: () async {
+                      // Call delete function
+                      await APIs.deleteMessage(message);
+                      Navigator.pop(context);
+                    },
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.green,
+                    ),
+                    title: Text(
+                        'Sent At: ${MyDateUtil.getFormattedTime(context: context, time: message.sent)}'),
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                        'Read At: ${message.read.isNotEmpty ? '${MyDateUtil.getMessageTime(context: context, time: message.read)}' : 'Not seen yet'}'),
+                  ),
+                ] else ...[
+                  ListTile(
+                    leading: const Icon(
+                      Icons.copy_all_rounded,
+                      color: Colors.blue,
+                    ),
+                    title: const Text('Copy'),
+                    onTap: () async {
+                      // Copy the message text to clipboard
+                      await Clipboard.setData(ClipboardData(text: message.msg));
+                      // Show a snackbar to confirm the copy action
+                      Navigator.pop(context); // Hide the bottom sheet
+                      Dialogs.showSnackbar(context, 'Message Copied!');
+                    },
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.green,
+                    ),
+                    title: Text(
+                        'Sent At: ${MyDateUtil.getFormattedTime(context: context, time: message.sent)}'),
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                        'Read At: ${message.read.isNotEmpty ? '${MyDateUtil.getMessageTime(context: context, time: message.read)}' : 'Not seen yet'}'),
+                  ),
+                ],
+              ],
             ),
           ),
         );
       },
     );
+  }
+
+  void _saveImage() {
+    // Implement the logic to save the image.
+    // This might include downloading the image from a URL and saving it to local storage.
   }
 
   void _showEditMessageDialog(BuildContext context) {
